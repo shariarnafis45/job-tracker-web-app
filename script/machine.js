1dbb12b54c297aa89a523d3a083c2bd367f4782a
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-02-22 17:32:30 +0600

    added dynamically updated fucntion for total , interview, rejected job count

--- script/machine.js
+++ script/machine.js
@@ -19,3 +19,33 @@ function removeHidden(id){
     const element = document.getElementById(id);
     element.classList.remove('hidden');
 }
+
+// Machine Calculate the number of total , interview , rejected
+
+function calculateAll(){
+    // total job numbers
+    const totalJobNums = document.querySelectorAll('.total-job-number');
+    const allCard = document.getElementById('all-card');
+    const totalJob = allCard.children.length;
+    for(let num of totalJobNums){
+        num.innerText = totalJob;
+    }
+    // total Interview job numbers
+    const totalInterviewJobNums = document.querySelectorAll('.total-interview-job');
+    const interviewJobCard = document.getElementById('interview-job-list');
+    const totalInterviewJob = interviewJobCard.children.length;
+    for(let num of totalInterviewJobNums){
+        num.innerText = totalInterviewJob;
+    }
+    
+    // total Reject job numbers
+    const totalRejectJobNums = document.querySelectorAll('.total-reject-job');
+    const rejectJobCard = document.getElementById('reject-job-list');
+    const totalRejectJob = rejectJobCard.children.length;
+    for(let num of totalRejectJobNums){
+        num.innerText = totalRejectJob;
+    }
+    
+}
+
+calculateAll();
